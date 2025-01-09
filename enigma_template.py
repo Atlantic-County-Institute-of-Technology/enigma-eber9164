63a27886ad5bfd097fa1e662f10a4aae6d468874
eber9164 <127509673+eber9164@users.noreply.github.com>
2025-01-09 15:58:00 -0500

    googa

--- enigma_template.py
+++ enigma_template.py
@@ -27,37 +27,85 @@ def encode_message():
 def encode_file():
     let = 0  # Is what letter it's checking
     result = ""  # The encoded file will be result
-    file = input("What file?: ")
-    file = open(file)
-    print(file)
+    filnam = input("What file?: ")
+    with open(filnam) as file:
+        filmes = file.read().lower()
+
     key = int(input("Key?: "))
-    for x in file:  # repeats code for each letter in the file
-        letter = alphabet.index(file[let])  # grabs the number that letter is equal to (Ex: 4 = d so if grabs 4)
-        result = result + alphabet[((letter + key) % 26)]  # %26 makes it if it at z(25) it goes back to a(0)
-        let = let + 1
+    for enletr in filmes:  # repeats code for each letter in the file
+        if enletr.isalpha():  # For (!, spaces, symbols) / not get mixed around in alphabet (Ex: ! becoming r)
+            letter = alphabet.index(enletr)  # grabs the number that letter is equal to (Ex: 4 = d so if grabs 4)
+            result = result + alphabet[((letter + key) % 26)]  # %26 makes it if it at z(25) it goes back to a(0)
+        else:
+            result = result + enletr
     print(result)
+
+    contin = int(input("[1] write to another file\n"
+                   "[2] Overwrite this file\n"
+                   "[3] No\n\n"
+                   "Answer: "))
+
+    if contin == 1:
+        filnam = input("What file?: ")
+        with open(filnam, 'w') as file: #  'w' is writing mode, allows for new file to be created if not already existing
+            file.write(result)
+
+    elif contin == 2:
+        with open(filnam, 'w') as file: #  'w' is writing mode, allows for new file to be created if not already existing
+            file.write(result)
+
+
+
 
 # decodes target file using a user-specified key. If key is unknown, a keypress should
 # call decode_unknown_key()
 def decode_file():
-    # with open( ) as file:
     let = 0  # Is what letter it's checking
     result = ""  # The encoded file will be result
-    file = input("What file?: ")
-    file = open(file)
-    key = 1
-    print(file)
-    for file in range(26):
-        for x in file:# repeats code for each letter in the file
-            letter = alphabet.index(file[let])  # grabs the number that letter is equal to (Ex: 4 = d so if grabs 4)
-            result = result + alphabet[((letter + key) % 26)]  # %26 makes it if it at z(25) it goes back to a(0)
-            let = let + 1
+    filnam = input("What file?: ")
+    with open(filnam) as file:
+        filmes = file.read().lower()
+
+    key = int(input("Key?: ") or -1)
+
+    if(key == -1):
+        decode_unknown_key(filmes)
+    else:
+
+        for enletr in filmes:  # repeats code for each letter in the file
+            if enletr.isalpha():  # For (!, spaces, symbols) / not get mixed around in alphabet (Ex: ! becoming r)
+                letter = alphabet.index(enletr)  # grabs the number that letter is equal to (Ex: 4 = d so if grabs 4)
+                result = result + alphabet[((letter - key) % 26)]  # %26 makes it if it at z(25) it goes back to a(0)
+            else:
+                result = result + enletr
         print(result)
-        key = key + 1
+
+        contin = int(input("[1] write to another file\n"
+                       "[2] Overwrite this file\n"
+                       "[3] No\n\n"
+                       "Answer: "))
+
+        if contin == 1:
+            filnam = input("What file?: ")
+            with open(filnam, 'w') as file: #  'w' is writing mode, allows for new file to be created if not already existing
+                file.write(result)
+
+        elif contin == 2:
+            with open(filnam, 'w') as file: #  'w' is writing mode, allows for new file to be created if not already existing
+                file.write(result)
 
 # runs if the key is unknown. If this is true, print out all possible decoding combinations.
-def decode_unknown_key(filename):
-   pass
+def decode_unknown_key(filmes):
+    result = ""
+    for key in range(26):
+        for enletr in filmes:  # repeats code for each letter in the file
+            if enletr.isalpha():  # For (!, spaces, symbols) / not get mixed around in alphabet (Ex: ! becoming r)
+                letter = alphabet.index(enletr)  # grabs the number that letter is equal to (Ex: 4 = d so if grabs 4)
+                result = result + alphabet[((letter - key) % 26)]  # %26 makes it if it at z(25) it goes back to a(0)
+            else:
+                result = result + enletr
+    print(f"The decode number is {key}.\n"
+          f"{result}\n")
 
 
 # main method declaration
@@ -87,3 +135,5 @@ def main():
 # runs on program start
 if __name__ == "__main__":
     main()
+
+
